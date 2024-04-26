06f25beb7da755a18e82eccabc06b9b08348f0ea
halil <tomahalil1@gmail.com>
2024-04-26 21:26:14 +0300

    inital

---
+++ app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+           <div className="card">
+
+           </div>
+        </div>
+    );
+};
+
+export default Page;
